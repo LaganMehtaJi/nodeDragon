4030de27d0bbdfa2953c1d29b898bd20e907d4f2
LaganMehtaJi <laganmehta.in@gmail.com>
2025-05-23 20:42:16 +0530

    Delete  All Project Due to Bugs

--- nodedragon/src/components/small.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-function small(props){
-    return(
-    <>
-    <button style={{fontSize:"16px",border:"1px solid green",width:"150px",height:"30px",borderRadius:"12px"}}>{props.name}</button>
-    </>
-    );
-}
-export default small;
